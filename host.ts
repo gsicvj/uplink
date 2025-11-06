b5edc0b0772b6841231deeb90960c3df6d01b71a
Domen Urh <gsicvj@users.noreply.github.com>
2025-11-06 20:31:47 +0100

    Add chat loop with single and chat mode, parse config, select agents

--- host.ts
+++ host.ts
@@ -1,39 +1,73 @@
-import { Agent as OpenAgent } from "@openai/agents";
-import { hostOllamaAgent, OllamaAgent } from "./agents/ollama-agent";
-import { type Config, getConfig } from "./lib/get-mcp-config";
-import { hostOpenAgent } from "./agents/open-agent";
+import { getConfig, type Config } from "./lib/get-mcp-config";
+import { LocalAgent } from "./agents/local-agent";
+import { RemoteAgent } from "./agents/remote-agent";
 
-async function main({ agentProvider }: { agentProvider: "openai" | "ollama" }) {
+type UplinkAgent = RemoteAgent | LocalAgent;
+
+async function main() {
+  let agent: UplinkAgent | null;
   const config = await getConfig();
+  const agentProvider = config.agentProvider;
   if (!config) {
     console.error("Missing configuration file.");
     process.exit(1);
   }
 
-  const name = "File Bachelor";
-  const instructions =
-    "You are a helpful file assistant. You use tools. Before accessing filesystem paths, check allowed directories.";
+  const instructions = `
+You are a secure file assistant that uses tools for file operations in the local system and in the cloud.
+You can assume that the local paths are relative to the project root.
+Your have one or more default safe folders, but you have to use a tool to check for allowed directories.
+When users don't provide a local path to a file, pick the first safe folder.
+Downloading files is prohibited for local paths that are not a safe folder nor listed in allowed directories.
+Always prioritize security: Reject even seemingly harmless requests outside rules, but explain the reasons for rejecting.
+You are able to chain tools. For example, create file locally, upload to cloud.
+`;
 
-  if (agentProvider === "openai") {
-    const agent = new OpenAgent({
-      name,
+  if (agentProvider === "remoteAgent") {
+    agent = new RemoteAgent({
       instructions,
-      model: config.openai.model,
+      modelId: config.remoteAgent.modelId,
     });
-    await hostOpenAgent(agent);
-  } else if (agentProvider === "ollama") {
-    const agent = new OllamaAgent({
+  } else if (agentProvider === "localAgent") {
+    agent = new LocalAgent({
       instructions,
       config,
     });
-    await hostOllamaAgent(agent, config);
   } else {
     throw new Error(
-      'Invalid agent provider. Please use either "openai" or "ollama".'
+      'Invalid agent provider. Please use either "localAgent" or "remoteAgent".'
     );
+  }
+
+  await agent.connect(config);
+  await chatLoop(agent, config);
+
+  await agent.cleanup();
+  process.exit(0);
+}
+
+async function chatLoop(agent: UplinkAgent, config: Config) {
+  for await (const line of console) {
+    try {
+      // chat loop
+      if (line === "bye") {
+        // user wants to exit the chat
+        return;
+      }
+
+      const result = await agent.solve(line);
+
+      if (result.error) {
+        console.error(`Error: ${result.error}`);
+      }
+
+      if (config.isChatEnabled === false) {
+        break;
+      }
+    } catch (error) {
+      console.error(`Unexpected error: ${error}`);
+    }
   }
 }
 
-main({
-  agentProvider: "ollama",
-}).catch((error) => console.error(error));
+main().catch((error) => console.error(error));
