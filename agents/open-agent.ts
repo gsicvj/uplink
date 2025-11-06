736550270db659327f351862577818b4612a0811
Domen Urh <gsicvj@users.noreply.github.com>
2025-11-06 20:32:22 +0100

    Cleanup

--- agents/open-agent.ts
+++
@@ -1,9 +0,0 @@
-/** This file is work in progress */
-
-export class OllamaAgent {
-  // TODO: implement this
-}
-
-export async function hostOpenAgent(agent: OllamaAgent) {
-  // TODO: implement this
-}
