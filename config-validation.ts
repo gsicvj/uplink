a057f3ff508fea1e2dd81cd597b769ccbe51f7e7
Domen Urh <gsicvj@users.noreply.github.com>
2026-03-10 11:24:09 +0700

    Add attributes and rename file for clarity

--- config-validation.ts
+++ config-validation.ts
@@ -8,7 +8,8 @@ import { z } from "zod";
 export const mcpServerSchema = z.object({
   command: z.string().min(1, "Missing command to run the MCP server."),
   args: z.array(z.string()).min(1, "Missing MCP server arguments."),
-  env: z.record(z.string()).optional(),
+  vargs: z.array(z.string()).describe("Additional arguments that the MCP server might need."),
+  env: z.record(z.string()).optional().describe("Environment variables needed by the MCP server, specified as key-value pairs."),
   roots: z.array(z.object({
     uri: z.string(),
     name: z.string()
@@ -17,20 +18,25 @@ export const mcpServerSchema = z.object({
 
 export const agentSchema = z.object({
   host: z.string().min(1, "Missing agent host.").describe("The URL of the model provider server."),
-  modelId: z.string().min(1, "Missing model id.").describe("The unique identifier for the agent model."),
+  modelId: z.string().min(1, "Missing model id.").describe("The unique identifier for the selected model."),
+  models: z.array(z.string().min(1, "Missing a proper model name.")).describe("The list of available model identifiers that the agent can use.").min(1)
 });
+
+export const providerOptionSchema = z.enum(["localAgent", "remoteAgent"]);
 
 export const configSchema = z.object({
   mcpServers: z.record(mcpServerSchema),
   localAgent: agentSchema,
   remoteAgent: agentSchema,
-  agentProvider: z.enum(["localAgent", "remoteAgent"]),
+  agentProvider: providerOptionSchema,
   isChatEnabled: z.boolean()
 });
 
 export type McpServerConfig = z.infer<typeof mcpServerSchema>;
 
 export type AgentConfig = z.infer<typeof agentSchema>;
+
+export type ProviderOption = z.infer<typeof providerOptionSchema>;
 
 export type McpConfig = z.infer<typeof configSchema>;
 
